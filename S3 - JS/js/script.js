5ba415f582e707a8c0b9fe759afc4be9f2a17edf
Dedpy <wassimbenr@gmail.com>
2023-07-14 11:58:21 +0100

    game logic

--- S3 - JS/js/script.js
+++ S3 - JS/js/script.js
@@ -1,13 +1,30 @@
 
 
 // read user name and year of birth
+// var name = prompt("entrer votre nom");
+// console.log(name);
+// var dateNaissance = prompt("entrer votre date naissance")
+// console.log(dateNaissance);
 // fonction: calculer l'age
-// boolean function: test age > 18 : True
+// function calculAge(x)
+// {
+//     return 2023-x;
+// }
+// console.log(calculAge(2000));
+
+// const age = 20;
+
 // for loop "happy birthday "
+    // for ( let i = 0; i < age; i++) {
+    //     console.log("Happy Birthday 🎂");
+    // }
 
 
 //queryselector
 //addEventListener btn // modify a display text
+var btn = document.querySelector("#bt");
+document.querySelector("#bt").hidden = true;
+console.log(btn);
 
 
 // let btn = document.createElement("button");
--- S4 - Advanced JS/js/script.js
+++ S4 - Advanced JS/js/script.js
@@ -1,2 +1,30 @@
 'use strict';
 
+var guessNumber = Math.floor( Math.random()*20) +1 ;
+console.log(guessNumber);
+
+//btn
+document.querySelector(".check")
+//msg
+document.querySelector(".message")
+//input
+document.querySelector(".guess")
+
+
+document.querySelector(".check").addEventListener('click',()=>{
+
+    var input =  document.querySelector(".guess").value;
+   
+    if  (input < guessNumber){
+        document.querySelector(".message").innerHTML="Number is low";
+    }
+    else if (input > guessNumber){
+        document.querySelector(".message").innerHTML="number is high";
+    }
+    else{
+        document.querySelector(".message").innerHTML="number is equal";
+        document.querySelector("body").style.backgroundColor = "green";
+    }
+      
+    
+})
